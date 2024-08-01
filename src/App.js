8a46b6bd1b49c4d7b3b55c6c917735ba2cfaa5ea
Mathias Melo <melo.mathias01@gmail.com>
2024-07-31 22:06:13 -0300

    Conversor Dolar-UY

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import {useRef} from "react";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+  const dolarRef=useRef();
+
+  const resultadoRef=useRef();
+
+
+
+  const calcular=()=>{
+
+    const dolarValor=parseFloat(dolarRef.current.value); // Valor actual ingresado por el usuario
+
+    const pesosUy=dolarValor*40.28;
+
+    resultadoRef.current.innerHTML=pesosUy.toFixed(2) + " $U";
+
+  
+
+  }
+ 
+ 
+ 
+  return <div>
+
+    <h1>Conversor DÓLAR-UY</h1>
+    <div className='valorUSD centrarElementos'>
+      <input type='text' ref={dolarRef}></input>
+      <img className='banderaUSA' src="/assets/usa.jpg" alt='usa'></img>
     </div>
-  );
+    <br/>
+    <button className='centrarBoton'onClick={calcular}><img className='banderaURU' src="/assets/uru.jpg" alt='uru'></img>Convertir</button>
+    <br/> 
+    <div className='centrarElementos resultado' ref={resultadoRef}></div>
+
+  </div>
+    
+  
 }
+
+
 
 export default App;
