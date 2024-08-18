5a3dce0b307778534d7d3b22abaacca9824bcd07
Mathias Melo <melo.mathias01@gmail.com>
2024-08-17 23:57:16 -0300

    Alerta de strings en input agregada

--- src/App.js
+++ src/App.js
@@ -1,39 +1,88 @@
 
 import './App.css';
 
-import {useRef} from "react";
+import {useRef, useState, useEffect} from "react";
 
 function App() {
  
+  const[valorCambio, setValorCambio]=useState(null);
+
   const dolarRef=useRef();
 
   const resultadoRef=useRef();
 
+ useEffect(()=> {                                               // Operación asíncrona en segundo plano a la vez que otra que está en primer plano con la API externa
 
+  const llamaApiCambio=async()=>{
+
+    try {
+    
+      const respuesta=await fetch("https://v6.exchangerate-api.com/v6/144b932c9f34e873cc4bff21/latest/USD");
+
+      const datos=await respuesta.json();
+
+      console.log(datos);
+
+      setValorCambio(datos.conversion_rates.UYU);
+
+
+    }catch (error) {
+      
+      console.error("Error al acceder a la API:", error);
+
+    }
+  
+
+  };
+  
+  llamaApiCambio();
+
+ }, []);
+
+ 
 
   const calcular=()=>{
 
+
+   
+      
     const dolarValor=parseFloat(dolarRef.current.value); // Valor actual ingresado por el usuario
 
-    const pesosUy=dolarValor*40.28;
+    if(typeof(dolarValor) === 'number' && !isNaN(dolarValor)){
 
-    resultadoRef.current.innerHTML=pesosUy.toFixed(2) + " $U";
+      const pesosUy=dolarValor*valorCambio;
+      resultadoRef.current.innerHTML=pesosUy.toFixed(2) + " $U";
+     
 
-  
+    }else{
+
+      alert("Error!.Favor de ingresar valores numéricos");
+
+    }
 
   }
+
+ /* document.getElementById("ingresaValor")
+    .addEventListener("onkeydown", function(event) {
+    if (event.key === 13) {
+      event.preventDefault();
+      document.getElementById("convertir").click();
  
+    }
+}); */
+ 
+
  
  
   return <div>
 
     <h1>Conversor DÓLAR-UY</h1>
     <div className='valorUSD centrarElementos'>
-      <input type='text' ref={dolarRef}></input>
+      <h2>Ingrese valor de cambio </h2><input id="ingresaValor" type='text' ref={dolarRef}></input>
       <img className='banderaUSA' src="/assets/usa.jpg" alt='usa'></img>
     </div>
     <br/>
-    <button className='centrarBoton'onClick={calcular}><img className='banderaURU' src="/assets/uru.jpg" alt='uru'></img>Convertir</button>
+    <button id="convertir" className='centrarBoton'onClick={calcular}><img className='banderaURU' src="/assets/uru.jpg" alt='uru'></img>Convertir</button>
     <br/> 
     <div className='centrarElementos resultado' ref={resultadoRef}></div>
 
